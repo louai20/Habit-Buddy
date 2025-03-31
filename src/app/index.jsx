50796f3c1f08c94a8aaf3115b99c58c688284244
louai20 <alayoubi@kth.se>
2025-03-31 19:37:46 +0200

    making user login pressit usin sessionStorage

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,13 +1,15 @@
 import { Provider, useDispatch } from 'react-redux';
-import { store } from '../store/store';
+import { store, persistor } from '../store/store';
 //import { Habits } from '../components/Habits';
 import  AuthPresenter  from "../presenters/authPresenter";
-
+import { PersistGate } from 'redux-persist/integration/react';
   
 export default function IndexPage() { 
   return (
     <Provider store={store}>
-      <AuthPresenter />
+      <PersistGate loading={null} persistor={persistor}>
+        <AuthPresenter />
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,12 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import habitsReducer from '../models/habitsSlice';
 import authReducer from '../models/authSlice';
 import weatherReducer from '../models/weatherSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storageSession from "redux-persist/lib/storage/session"; // Use sessionStorage instead of localStorage
+
+// Persist config for Redux Persist
+const persistConfig = {
+  key: 'root',    // The key in storage
+  storage: storageSession,        // Use localStorage for persistence
+  whitelist: ["user"],
+  blacklist: ['auth.register'],
+};
+// Apply persistReducer only to the auth slices
+const persistedAuthReducer = persistReducer(persistConfig, (authReducer));
+
+// Combine all reducers
+const rootReducer = combineReducers({
+  habits: habitsReducer,
+  auth: persistedAuthReducer,
+  weather: weatherReducer,
+});
+
 
 export const store = configureStore({
-  reducer: {
-    habits: habitsReducer,
-    auth: authReducer,
-    weather: weatherReducer,
-  },
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore specific action types or paths in the state
+        ignoredActions: ['persist/PERSIST'],
+        ignoredPaths: ['auth.user'], // Ignore checking 'user' in auth
+      },
+    }),
 }); 
+
+export const persistor = persistStore(store);
+
