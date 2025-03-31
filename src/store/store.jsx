4f750200d0507c594093759cb0a36147b4db9ab9
louai20 <alayoubi@kth.se>
2025-03-31 17:29:29 +0200

    add weatherapi fetch call

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import habitsReducer from '../models/habitsSlice';
 import authReducer from '../models/authSlice';
+import weatherReducer from '../models/weatherSlice';
 
 export const store = configureStore({
   reducer: {
     habits: habitsReducer,
-    auth: authReducer
+    auth: authReducer,
+    weather: weatherReducer,
   },
 }); 
