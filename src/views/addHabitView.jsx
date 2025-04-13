fa81453f616b371c0c8fcdd08e20b1af6b9cad5f
Jiayiiing <studentjiayi@gmail.com>
2025-04-13 21:09:01 +0200

    remove datepicker

--- src/views/addHabitView.jsx
+++ src/views/addHabitView.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import DatePicker from 'react-native-date-picker';
-import DateTimePicker from '@react-native-community/datetimepicker';
 import {
   View,
   Text,
